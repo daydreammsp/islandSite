bbc22fd5cf1a5c1b81cbe20230bb3edec60b3c80
Jonathan Kruse <jonathanskruse@gmail.com>
2018-10-14 11:47:10 -0400

    favicon

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'jon');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'cleardb-metric-75145');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
